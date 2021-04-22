3b070b172bffcf45dd5ebec6be98bc3a9883d184
nreinick <nicholas.reinicke@nrel.gov>
2021-04-22 14:42:41 -0600

    update plotting function
    Former-commit-id: 15cccc08ca36152235267a80163573037e1bf4a0

--- yamm/utils/plot.py
+++ yamm/utils/plot.py
@@ -1,11 +1,10 @@
 from typing import List
 
 import folium
-
-import pandas as pd
 import geopandas as gpd
-
-from pyproj import Transformer
+import numpy as np
+import pandas as pd
+from shapely.geometry import Point
 
 from yamm.constructs.match import Match
 from yamm.constructs.trace import Trace
@@ -13,16 +12,18 @@ from yamm.maps.map_interface import MapInterface
 from yamm.utils.crs import XY_CRS, LATLON_CRS
 
 
-def plot_matches(trace: Trace, matches: List[Match], road_map: MapInterface):
+def plot_matches(trace: Trace, matches: List[Match], road_map: MapInterface, npoints: int):
     """
     plots a trace and the relevant matches on a folium map
 
     :param trace: the trace
     :param matches: the matches
     :param road_map: the road map
+    :param npoints: how many trace points to plot? useful in you have a large trace
 
     :return: the folium map
     """
+
     def match_to_road(m):
         d = {}
 
@@ -39,25 +40,37 @@ def plot_matches(trace: Trace, matches: List[Match], road_map: MapInterface):
 
         return d
 
-    df = pd.DataFrame([match_to_road(m) for m in matches])
-    df = df.loc[df.road_id.shift() != df.road_id]
-    gdf = gpd.GeoDataFrame(df, geometry=df.geom, crs=XY_CRS).drop(columns=["geom"])
-    gdf = gdf.to_crs(LATLON_CRS)
+    def match_to_coord(m):
+        d = {}
 
-    mid_i = int(len(trace.coords) / 2)
+        d['road_id'] = m.road.road_id
+        d['geom'] = Point(m.coordinate.x, m.coordinate.y)
+        d['distance'] = m.distance
+        return d
+
+    road_df = pd.DataFrame([match_to_road(m) for m in matches])
+    road_df = road_df.loc[road_df.road_id.shift() != road_df.road_id]
+    road_gdf = gpd.GeoDataFrame(road_df, geometry=road_df.geom, crs=XY_CRS).drop(columns=["geom"])
+    road_gdf = road_gdf.to_crs(LATLON_CRS)
+
+    coord_df = pd.DataFrame([match_to_coord(m) for m in matches])
+    index = [i for i in np.linspace(1, len(coord_df.index) - 1, npoints - 2).astype(int)]
+    coord_df = coord_df.loc[index]
+
+    coord_gdf = gpd.GeoDataFrame(coord_df, geometry=coord_df.geom, crs=XY_CRS).drop(columns=["geom"])
+    coord_gdf = coord_gdf.to_crs(LATLON_CRS)
+
+    mid_i = int(len(trace) / 2)
     mid_coord = trace.coords[mid_i].to_crs(LATLON_CRS)
 
     fmap = folium.Map(location=[mid_coord.x, mid_coord.y], zoom_start=11)
 
-    xs = [c.x for c in trace.downsample(1000).coords]
-    ys = [c.y for c in trace.downsample(1000).coords]
-    transformer = Transformer.from_crs(XY_CRS, LATLON_CRS)
-    lats, lons = transformer.transform(xs, ys)
+    for coord in coord_gdf.itertuples():
+        folium.Circle(location=(coord.geometry.y, coord.geometry.x), radius=5,
+                      tooltip=f"road_id: {coord.road_id}\ndistance: {coord.distance}").add_to(fmap)
 
-    for lat, lon in zip(lats, lons):
-        folium.Circle(location=(lat, lon), radius=5).add_to(fmap)
-
-    for road in gdf.itertuples():
-        folium.PolyLine([(lat, lon) for lon, lat in road.geometry.coords], color="red").add_to(fmap)
+    for road in road_gdf.itertuples():
+        folium.PolyLine([(lat, lon) for lon, lat in road.geometry.coords], color="red", tooltip=road.road_id).add_to(
+            fmap)
 
     return fmap
