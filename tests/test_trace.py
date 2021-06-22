3929df666bdd19b0c2226c365c1f59923be85914
nreinick <nicholas.reinicke@nrel.gov>
2021-06-22 15:47:00 -0600

    add from_datframe method for trace
    Former-commit-id: 63cbdf859bc07ca04feb3a963b39d9abd10ec111

--- tests/test_trace.py
+++ tests/test_trace.py
@@ -1,7 +1,28 @@
+from unittest import TestCase
+
+import pandas as pd
+
+from yamm import root
 from yamm.constructs.trace import Trace
+from yamm.utils.crs import XY_CRS
 
-trace = Trace.from_csv("../py-notebooks/Sample-2.csv")
 
-trace = trace.downsample(1000)
+class TestTrace(TestCase):
 
-print(len(trace))
+    def test_trace_from_file(self):
+        file = root() / "resources" / "traces" / "sample_trace_1.csv"
+
+        trace = Trace.from_csv(file)
+
+        self.assertEqual(trace.crs, XY_CRS)
+        self.assertEqual(len(trace), 949)
+
+    def test_trace_from_dataframe(self):
+        file = root() / "resources" / "traces" / "sample_trace_1.csv"
+
+        df = pd.read_csv(file)
+
+        trace = Trace.from_dataframe(df)
+
+        self.assertEqual(trace.crs, XY_CRS)
+        self.assertEqual(len(trace), 949)
--- yamm/constructs/trace.py
+++ yamm/constructs/trace.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
-from csv import DictReader
 from pathlib import Path
 from typing import List, Optional, Union
 
 import numpy as np
+import pandas as pd
 from pyproj import CRS, Transformer
 
 from yamm.constructs.coordinate import Coordinate
@@ -37,12 +37,47 @@ class Trace:
         return len(self.coords)
 
     @classmethod
+    def from_dataframe(
+            cls,
+            dataframe: pd.DataFrame,
+            xy: bool = True,
+            lat_column: str = "latitude",
+            lon_column: str = "longitude",
+    ) -> Trace:
+        """
+        Builds a trace from a pandas dataframe
+
+        Expects the dataframe to have latitude / longitude information in the epsg 4326 format
+
+        Automatically projects each coordinate to epsg 3857 as well
+
+        :param dataframe: pandas dataframe with _one_ trace
+        :param lat_column: which column to use for latitude
+        :param lon_column: which column to use for longitude
+        :param xy: should the trace be projected to epsg 3857?
+
+        :return: the trace built from the dataframe
+        """
+        lats = dataframe[lat_column]
+        lons = dataframe[lon_column]
+        if xy:
+            transformer = Transformer.from_crs(LATLON_CRS, XY_CRS)
+            lat_proj, lon_proj = transformer.transform(lats, lons)
+            coords = [Coordinate.from_xy(x, y) for x, y in zip(lat_proj, lon_proj)]
+            crs = XY_CRS
+        else:
+            coords = [Coordinate.from_latlon(lat, lon) for lat, lon in zip(lats, lons)]
+            crs = LATLON_CRS
+
+        return Trace(coords, crs)
+
+    @classmethod
     def from_csv(
             cls,
             file: Union[str, Path],
             xy: bool = True,
-            lat_column: Optional[str] = None,
-            lon_column: Optional[str] = None,
+            lat_column: str = 'latitude',
+            lon_column: str = 'longitude',
     ) -> Trace:
         """
         Builds a trace from a csv file.
@@ -64,53 +99,9 @@ class Trace:
         elif not filepath.suffix == ".csv":
             raise TypeError(f"file of type {filepath.suffix} does not appear to be a csv file")
 
-        lats = []
-        lons = []
-        with filepath.open('r') as f:
-            reader = DictReader(f)
+        df = pd.read_csv(filepath)
 
-            lat_name_set = valid_latitude_names.intersection(set(reader.fieldnames))
-            lon_name_set = valid_longitude_names.intersection(set(reader.fieldnames))
-
-            if lat_column:
-                lat_name = lat_column
-            elif len(lat_name_set) == 0:
-                raise ValueError(f"could not find latitude information from fields {set(reader.fieldnames)}")
-            elif len(lat_name_set) > 1:
-                raise ValueError("found multiple instances of latitude; please only provide one")
-            else:
-                lat_name = list(lat_name_set)[0]
-
-            if lon_column:
-                lon_name = lon_column
-            elif len(lon_name_set) == 0:
-                raise ValueError(f"could not find longitude information from fields {set(reader.fieldnames)}")
-            elif len(lon_name_set) > 1:
-                raise ValueError("found multiple instances of longitude; please only provide one")
-            else:
-                lon_name = list(lon_name_set)[0]
-
-            for row in reader:
-                lats.append(float(row[lat_name]))
-                lons.append(float(row[lon_name]))
-
-        if xy:
-            transformer = Transformer.from_crs(LATLON_CRS, XY_CRS)
-
-            lat_proj, lon_proj = transformer.transform(lats, lons)
-
-            coords = [Coordinate.from_xy(x, y) for x, y in zip(lat_proj, lon_proj)]
-            crs = XY_CRS
-        else:
-            coords = [Coordinate.from_latlon(lat, lon) for lat, lon in zip(lats, lons)]
-            crs = LATLON_CRS
-
-        trace = Trace(
-            coords=coords,
-            crs=crs
-        )
-
-        return trace
+        return Trace.from_dataframe(df, xy, lat_column, lon_column)
 
     def downsample(self, npoints: int) -> Trace:
         new_coords = [self.coords[0]] + [self.coords[i] for i in
