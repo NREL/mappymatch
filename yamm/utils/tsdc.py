39d14dc5abe232eacc1853207c501b3210d11e3e
nreinick <nicholas.reinicke@nrel.gov>
2021-09-02 10:12:31 -0600

    update match_tsdc.py to use new trip and point ids
    Former-commit-id: 0a382f1ee956b05750829e89181f3e0c89834dbb

--- yamm/utils/tsdc.py
+++ yamm/utils/tsdc.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import geopandas as gpd
 import pandas as pd
 from shapely.geometry import box
@@ -7,21 +9,21 @@ from yamm.constructs.trace import Trace
 from yamm.utils.crs import LATLON_CRS
 
 
-def get_tsdc_trace(sampno, vehno, table, engine):
+def get_tsdc_trace(trip_id, table, engine) -> Tuple[Trace, pd.DataFrame]:
     q = f"""
     select * from {table}  
-    where sampno={sampno} and vehno={vehno}
-    order by time_local
+    where trip_id='{trip_id}'
+    order by time_rel
     """
     try:
-        trip_gdf = gpd.GeoDataFrame.from_postgis(q, engine)
-        trip_gdf = trip_gdf.set_index(['sampno', 'vehno', 'time_local'])
-        return Trace.from_geo_dataframe(trip_gdf)
+        raw_trip_gdf = gpd.GeoDataFrame.from_postgis(q, engine)
+        trip_gdf = raw_trip_gdf.set_index(['point_id', 'time_rel'])
+        return Trace.from_geo_dataframe(trip_gdf), raw_trip_gdf
     except ValueError:
         # table might not have geometry
-        trip_df = pd.read_sql(q, engine)
-        trip_df = trip_df.set_index(['sampno', 'vehno', 'time_local'])
-        return Trace.from_dataframe(trip_df, lat_column="latitude", lon_column="longitude")
+        raw_trip_df = pd.read_sql(q, engine)
+        trip_df = raw_trip_df.set_index(['point_id', 'time_local'])
+        return Trace.from_dataframe(trip_df, lat_column="latitude", lon_column="longitude"), raw_trip_df
 
 
 def compute_bbox_from_table(table, padding, engine):
@@ -40,8 +42,7 @@ def compute_bbox_from_table(table, padding, engine):
 
 def get_unique_trips(table, engine):
     q = f"""
-    select sampno, vehno from {table} 
-    group by sampno, vehno
+    select distinct trip_id from {table} 
     """
     trips = pd.read_sql(q, engine)
     trips['table'] = table
