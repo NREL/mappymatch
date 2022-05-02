385772176d812e0dd996a9d8b9cfa4eef35d5d1d
Jason Sexauer <genericcarbonlifeform@gmail.com>
2022-05-02 11:41:30 -0400

    Add an assert to the test_osm

--- tests/test_osm.py
+++ tests/test_osm.py
@@ -1,15 +1,22 @@
 from unittest import TestCase, skip
 
 from tests import get_test_dir
+from yamm.constructs.coordinate import Coordinate
 from yamm.constructs.geofence import Geofence
 from yamm.maps.nx.readers.osm_readers import read_osm_nxmap
+from yamm.utils.crs import XY_CRS
 
 
 class TestOSMap(TestCase):
-    @skip
+
     def test_osm_networkx_graph(self):
         gfile = get_test_dir() / "test_assets" / "downtown_denver.geojson"
 
         geofence = Geofence.from_geojson(gfile)
 
         osm_map = read_osm_nxmap(geofence)
+
+        # Make sure we can find East 23rd Ave within the Denver Zoo.
+        denver_zoo = Coordinate.from_lat_lon(39.751029, -104.956666)
+        e23_ave = osm_map.nearest_road(denver_zoo.to_crs(XY_CRS))
+        self.assertEqual(e23_ave.road_id, '176101784-176102601')
