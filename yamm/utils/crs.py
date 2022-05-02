32bb5b3d53d3903724bccba41a8521fa295fa73b
Graham Waters <63750901+icehelmetminer@users.noreply.github.com>
2022-05-02 15:28:29 -0600

    added function to swap LATLON_CRS TO XY_CRS and vice verca

--- yamm/utils/crs.py
+++ yamm/utils/crs.py
@@ -2,3 +2,25 @@ from pyproj import CRS
 
 LATLON_CRS = CRS(4326)
 XY_CRS = CRS(3857)
+
+def change_crs(current_mode):
+    """
+    change from latlon to xy_crs or the other way.
+
+    Args:
+        current_mode (_type_): _description_
+
+    Raises:
+        ValueError: _description_
+
+    Returns:
+        _type_: _description_
+    """
+    if str(current_mode) == "LATLON_CRS":
+        current_mode = XY_CRS
+    elif str(current_mode) == "XY_CRS":
+        current_mode = LATLON_CRS
+    else:
+        raise ValueError("CRS is not LatLon_CRS or XY_CRS")
+    
+    return current_mode
