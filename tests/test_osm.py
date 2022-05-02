4208b3445532ffef6fea058ad3b6156fa1eb8f52
Jason Sexauer <genericcarbonlifeform@gmail.com>
2022-05-02 13:41:48 -0400

    flip x/y in coordinate

--- tests/test_osm.py
+++ tests/test_osm.py
@@ -4,7 +4,7 @@ from tests import get_test_dir
 from yamm.constructs.coordinate import Coordinate
 from yamm.constructs.geofence import Geofence
 from yamm.maps.nx.readers.osm_readers import read_osm_nxmap
-from yamm.utils.crs import XY_CRS
+from yamm.utils.crs import XY_CRS, LATLON_CRS
 
 
 class TestOSMap(TestCase):
@@ -16,16 +16,19 @@ class TestOSMap(TestCase):
 
         osm_map = read_osm_nxmap(geofence)
 
-        # Make sure we can find East 23rd Ave within the Denver Zoo.
-        denver_zoo = Coordinate.from_lat_lon(39.751029, -104.956666)
-        e23_ave = osm_map.nearest_road(denver_zoo.to_crs(XY_CRS))
-        self.assertEqual(e23_ave.road_id, '176101784-176102601')
+        # Make sure we can find 31st St near Mestizo-Curtis Park
+        mestizo_curtis_park = Coordinate.from_lat_lon(39.761613, -104.977189)
+        st31 = osm_map.nearest_road(mestizo_curtis_park.to_crs(XY_CRS))
+        # DEBUG: plot the street it choose using geojson.io
+        #import geopandas
+        #print(geopandas.GeoSeries(st31.geom).set_crs(XY_CRS).to_crs(LATLON_CRS).to_json())
+        self.assertEqual(st31.road_id, '1321042414-3323569423')
 
         # Make sure the graph contains this road segment
-        e23_ave_node1 = osm_map.g.nodes[176101784]
-        e23_ave_node2 = osm_map.g.nodes[176102601]
-        self.assertAlmostEqual(e23_ave_node1['lat'], 39.7578861, 5)
-        self.assertAlmostEqual(e23_ave_node1['lon'], -104.9844841, 5)
-        self.assertAlmostEqual(e23_ave_node2['lat'], 39.7585533, 5)
-        self.assertAlmostEqual(e23_ave_node2['lon'], -104.9853503, 5)
-        self.assertAlmostEqual(osm_map.g.get_edge_data(176101784, 176102601, 0)['kilometers'], 0.104817, 5)
+        e23_ave_node1 = osm_map.g.nodes[1321042414]
+        e23_ave_node2 = osm_map.g.nodes[3323569423]
+        self.assertAlmostEqual(e23_ave_node1['lat'], 39.761877, 5)
+        self.assertAlmostEqual(e23_ave_node1['lon'], -104.9775733, 5)
+        self.assertAlmostEqual(e23_ave_node2['lat'], 39.7602886, 5)
+        self.assertAlmostEqual(e23_ave_node2['lon'], -104.9779113, 5)
+        self.assertAlmostEqual(osm_map.g.get_edge_data(1321042414, 3323569423, 0)['kilometers'], 0.246787, 5)
