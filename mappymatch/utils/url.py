b0d2c1125742b908d1c3ebece5bb6d75a7aa068a
Edward G <edward.g2013@gmail.com>
2022-05-03 14:33:46 -0700

    Create docstrings for utils/url.py .

--- mappymatch/utils/url.py
+++ mappymatch/utils/url.py
@@ -1,12 +1,24 @@
-import functools as ft
+from functools import reduce
 from typing import List
 from urllib.parse import urljoin
 
 
 def _parse_uri(uri: str) -> str:
+    """Internal use."""
     return uri if uri.endswith("/") else f"{uri}/"
 
 
 def multiurljoin(urls: List[str]) -> str:
+    """
+    Make a url from uri's.
+
+    TODO: Maintainer check
+
+    Args:
+        urls: list of uri
+
+    Returns:
+        Url as uri/uri/...
+    """
     parsed_urls = [_parse_uri(uri) for uri in urls]
-    return ft.reduce(urljoin, parsed_urls)
+    return reduce(urljoin, parsed_urls)
