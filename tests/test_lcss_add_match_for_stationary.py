024c21effe937b28b6feb0de874b5ef3eac278dd
Scott Irwin <sirwin@rocketmail.com>
2022-05-03 13:10:11 -0600

    test cases for adding a single stationary point

--- tests/test_lcss_add_match_for_stationary.py
+++ tests/test_lcss_add_match_for_stationary.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
+from cmath import exp
 from copy import deepcopy
 from unittest import TestCase
 
-from pyproj import CRS
 from shapely.geometry import LineString
-from shapely.geometry import Point
 
 from mappymatch.constructs.coordinate import Coordinate
 from mappymatch.constructs.match import Match
@@ -16,8 +15,6 @@ class TestLCSSAddMatchForStationary(TestCase):
     def test_add_matches_no_stationary_points(self):
         ''' This will test the "null case" which is to say, no stationary points
         '''
-        stationary_index: list[StationaryIndex] = []
-
         # Road(id: str|int, geom: LineString, metadata: dict)
         roads: list[Road] = [
             Road("first st", LineString(), {}),
@@ -37,13 +34,142 @@ class TestLCSSAddMatchForStationary(TestCase):
             (39.656707,-104.919964),
         ]
         coords: list[Coordinate] = [Coordinate.from_lat_lon(lat, lon) for lat, lon in lat_longs]
-
         # Match(road, coordinate, distance)
         matches: list[Match] = [Match(r, c, 0.1) for r, c in zip(roads, coords)]
 
+        # ensure that the expected matches are different from the matches that will be passed in
         expected_matches = deepcopy(matches)
+
+        stationary_index: list[StationaryIndex] = []
 
         resulting_matches = add_matches_for_stationary_points(matches, stationary_index)
 
         self.assertListEqual(expected_matches, resulting_matches)
 
+    def test_add_matches_one_stationary_point_at_beginning(self):
+        ''' Test adding a single stationary point at the beginning
+        '''
+        # Road(id: str|int, geom: LineString, metadata: dict)
+        roads: list[Road] = [
+            Road("first st", LineString(), {}),
+            Road("second st", LineString(), {}),
+            Road("main st", LineString(), {}),
+            Road("second str", LineString(), {}),
+            Road(123, LineString(), {}),
+            Road(234, LineString(), {}),
+        ]
+
+        lat_longs = [
+            (39.655193,-104.919294),
+            (39.655494,-104.91943),
+            (39.655801,-104.919567),
+            (39.656103,-104.919698),
+            (39.656406,-104.919831),
+            (39.656707,-104.919964),
+        ]
+        coords: list[Coordinate] = [Coordinate.from_lat_lon(lat, lon) for lat, lon in lat_longs]
+        # Match(road, coordinate, distance)
+        matches: list[Match] = [Match(r, c, 0.1) for r, c in zip(roads, coords)]
+
+        # ensure that the expected matches are different from the matches that will be passed in
+        expected_matches = deepcopy(matches)
+        # now, add the expected points
+        m = expected_matches[0]
+        new_m = m.set_coordinate(Coordinate('new', m.coordinate.geom, m.coordinate.crs))
+        expected_matches.insert(0, new_m)
+
+        # StationaryIndex( i_index: List[int], c_index: List[Any])
+        stationary_index: list[StationaryIndex] = [StationaryIndex([0,0], [None,'new'])]
+
+        resulting_matches = add_matches_for_stationary_points(matches, stationary_index)
+
+        self.assertListEqual(expected_matches, resulting_matches)
+
+    def test_add_matches_one_stationary_point_at_end(self):
+        ''' Test adding a single stationary point at the end
+        '''
+        # Road(id: str|int, geom: LineString, metadata: dict)
+        roads: list[Road] = [
+            Road("first st", LineString(), {}),
+            Road("second st", LineString(), {}),
+            Road("main st", LineString(), {}),
+            Road("second str", LineString(), {}),
+            Road(123, LineString(), {}),
+            Road(234, LineString(), {}),
+        ]
+
+        lat_longs = [
+            (39.655193,-104.919294),
+            (39.655494,-104.91943),
+            (39.655801,-104.919567),
+            (39.656103,-104.919698),
+            (39.656406,-104.919831),
+            (39.656707,-104.919964),
+        ]
+        coords: list[Coordinate] = [Coordinate.from_lat_lon(lat, lon) for lat, lon in lat_longs]
+        # Match(road, coordinate, distance)
+        matches: list[Match] = [Match(r, c, 0.1) for r, c in zip(roads, coords)]
+
+        # ensure that the expected matches are different from the matches that will be passed in
+        expected_matches = deepcopy(matches)
+        # now, add the expected points
+        m = expected_matches[-1]
+        new_m = m.set_coordinate(Coordinate('new', m.coordinate.geom, m.coordinate.crs))
+        expected_matches.append(new_m)
+
+        # StationaryIndex( i_index: List[int], c_index: List[Any])
+        stationary_index: list[StationaryIndex] = [StationaryIndex([-1,len(matches)], [None,'new'])]
+
+        resulting_matches = add_matches_for_stationary_points(matches, stationary_index)
+
+        self.assertListEqual(expected_matches, resulting_matches)
+
+    def test_add_matches_one_stationary_point_in_middle(self):
+        ''' Test adding a single stationary point in the middle
+        '''
+        # Road(id: str|int, geom: LineString, metadata: dict)
+        roads: list[Road] = [
+            Road("first st", LineString(), {}),
+            Road("second st", LineString(), {}),
+            Road("main st", LineString(), {}),
+            Road("second str", LineString(), {}),
+            Road(123, LineString(), {}),
+            Road(234, LineString(), {}),
+        ]
+
+        lat_longs = [
+            (39.655193,-104.919294),
+            (39.655494,-104.91943),
+            (39.655801,-104.919567),
+            (39.656103,-104.919698),
+            (39.656406,-104.919831),
+            (39.656707,-104.919964),
+        ]
+        coords: list[Coordinate] = [Coordinate.from_lat_lon(lat, lon) for lat, lon in lat_longs]
+        # Match(road, coordinate, distance)
+        matches: list[Match] = [Match(r, c, 0.1) for r, c in zip(roads, coords)]
+
+        # ensure that the expected matches are different from the matches that will be passed in
+        expected_matches = deepcopy(matches)
+        # now, add the expected points
+        m = expected_matches[-1]
+        new_m = m.set_coordinate(Coordinate('new', m.coordinate.geom, m.coordinate.crs))
+        expected_matches[-1:-1] = [new_m]
+
+        # StationaryIndex( i_index: List[int], c_index: List[Any])
+        stationary_index: list[StationaryIndex] = [StationaryIndex([-1,-1], [None,'new'])]
+
+        resulting_matches = add_matches_for_stationary_points(matches, stationary_index)
+
+        self.assertListEqual(expected_matches, resulting_matches)
+
+        expected_matches = deepcopy(matches)
+        indx = len(matches) // 2
+        m = expected_matches[indx]
+        new_m = m.set_coordinate(Coordinate('new', m.coordinate.geom, m.coordinate.crs))
+        expected_matches.insert(indx, new_m)
+        stationary_index: list[StationaryIndex] = [StationaryIndex([indx, indx], [None,'new'])]
+
+        resulting_matches = add_matches_for_stationary_points(matches, stationary_index)
+
+        self.assertListEqual(expected_matches, resulting_matches)
