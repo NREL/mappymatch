251df74191205848e396fd10cab91bb201f1d3f2
Edward G <edward.g2013@gmail.com>
2022-05-03 10:09:29 -0700

    Run precommit with black against the codebase.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,15 +12,16 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
-print(os.path.abspath('../..'))
+
+sys.path.insert(0, os.path.abspath("../.."))
+print(os.path.abspath("../.."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'Mappymatch'
-copyright = '2022, National Renewable Energy Laboratory'
-author = 'National Renewable Energy Laboratory'
+project = "Mappymatch"
+copyright = "2022, National Renewable Energy Laboratory"
+author = "National Renewable Energy Laboratory"
 
 
 # -- General configuration ---------------------------------------------------
@@ -29,14 +30,14 @@ author = 'National Renewable Energy Laboratory'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx_rtd_theme',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.viewcode',
+    "sphinx.ext.autodoc",
+    "sphinx_rtd_theme",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -49,9 +50,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
