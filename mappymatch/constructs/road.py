a3764e4c10b56b98c21b03a8e78097470b682ab8
Scott Irwin <sirwin@rocketmail.com>
2022-05-03 15:39:01 -0600

    fix isort errors

--- mappymatch/constructs/road.py
+++ mappymatch/constructs/road.py
@@ -9,7 +9,9 @@ class Road(NamedTuple):
     road_id: Union[int, str]
 
     geom: LineString
-    origin_junction_id: Union[int, str]  # The id of the previous road in the path
+    origin_junction_id: Union[
+        int, str
+    ]  # The id of the previous road in the path
     dest_junction_id: Union[int, str]  # The id of the next road in the path
     # holds any info specific to the road network
     metadata: Optional[dict] = None
--- tests/test_lcss_add_match_for_stationary.py
+++ tests/test_lcss_add_match_for_stationary.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+
 from cmath import exp
 from copy import deepcopy
 from unittest import TestCase
@@ -8,13 +9,15 @@ from shapely.geometry import LineString
 from mappymatch.constructs.coordinate import Coordinate
 from mappymatch.constructs.match import Match
 from mappymatch.constructs.road import Road
-from mappymatch.matchers.lcss.ops import StationaryIndex
-from mappymatch.matchers.lcss.ops import add_matches_for_stationary_points
+from mappymatch.matchers.lcss.ops import (
+    StationaryIndex,
+    add_matches_for_stationary_points,
+)
+
 
 class TestLCSSAddMatchForStationary(TestCase):
     def test_add_matches_no_stationary_points(self):
-        ''' This will test the "null case" which is to say, no stationary points
-        '''
+        """This will test the "null case" which is to say, no stationary points"""
         # Road(id: str|int, geom: LineString, metadata: dict)
         roads = [
             Road("first st", LineString(), {}),
@@ -26,12 +29,12 @@ class TestLCSSAddMatchForStationary(TestCase):
         ]
 
         lat_longs = [
-            (39.655193,-104.919294),
-            (39.655494,-104.91943),
-            (39.655801,-104.919567),
-            (39.656103,-104.919698),
-            (39.656406,-104.919831),
-            (39.656707,-104.919964),
+            (39.655193, -104.919294),
+            (39.655494, -104.91943),
+            (39.655801, -104.919567),
+            (39.656103, -104.919698),
+            (39.656406, -104.919831),
+            (39.656707, -104.919964),
         ]
         coords = [Coordinate.from_lat_lon(lat, lon) for lat, lon in lat_longs]
         # Match(road, coordinate, distance)
@@ -42,13 +45,14 @@ class TestLCSSAddMatchForStationary(TestCase):
 
         stationary_index: list[StationaryIndex] = []
 
-        resulting_matches = add_matches_for_stationary_points(matches, stationary_index)
+        resulting_matches = add_matches_for_stationary_points(
+            matches, stationary_index
+        )
 
         self.assertListEqual(expected_matches, resulting_matches)
 
     def test_add_matches_one_stationary_point_at_beginning(self):
-        ''' Test adding a single stationary point at the beginning
-        '''
+        """Test adding a single stationary point at the beginning"""
         # Road(id: str|int, geom: LineString, metadata: dict)
         roads = [
             Road("first st", LineString(), {}),
@@ -60,12 +64,12 @@ class TestLCSSAddMatchForStationary(TestCase):
         ]
 
         lat_longs = [
-            (39.655193,-104.919294),
-            (39.655494,-104.91943),
-            (39.655801,-104.919567),
-            (39.656103,-104.919698),
-            (39.656406,-104.919831),
-            (39.656707,-104.919964),
+            (39.655193, -104.919294),
+            (39.655494, -104.91943),
+            (39.655801, -104.919567),
+            (39.656103, -104.919698),
+            (39.656406, -104.919831),
+            (39.656707, -104.919964),
         ]
         coords = [Coordinate.from_lat_lon(lat, lon) for lat, lon in lat_longs]
         # Match(road, coordinate, distance)
@@ -75,19 +79,22 @@ class TestLCSSAddMatchForStationary(TestCase):
         expected_matches = deepcopy(matches)
         # now, add the expected points
         m = expected_matches[0]
-        new_m = m.set_coordinate(Coordinate('new', m.coordinate.geom, m.coordinate.crs))
+        new_m = m.set_coordinate(
+            Coordinate("new", m.coordinate.geom, m.coordinate.crs)
+        )
         expected_matches.insert(0, new_m)
 
         # StationaryIndex( i_index: List[int], c_index: List[Any])
-        stationary_index = [StationaryIndex([0,0], [None,'new'])]
+        stationary_index = [StationaryIndex([0, 0], [None, "new"])]
 
-        resulting_matches = add_matches_for_stationary_points(matches, stationary_index)
+        resulting_matches = add_matches_for_stationary_points(
+            matches, stationary_index
+        )
 
         self.assertListEqual(expected_matches, resulting_matches)
 
     def test_add_matches_one_stationary_point_at_end(self):
-        ''' Test adding a single stationary point at the end
-        '''
+        """Test adding a single stationary point at the end"""
         # Road(id: str|int, geom: LineString, metadata: dict)
         roads = [
             Road("first st", LineString(), {}),
@@ -99,12 +106,12 @@ class TestLCSSAddMatchForStationary(TestCase):
         ]
 
         lat_longs = [
-            (39.655193,-104.919294),
-            (39.655494,-104.91943),
-            (39.655801,-104.919567),
-            (39.656103,-104.919698),
-            (39.656406,-104.919831),
-            (39.656707,-104.919964),
+            (39.655193, -104.919294),
+            (39.655494, -104.91943),
+            (39.655801, -104.919567),
+            (39.656103, -104.919698),
+            (39.656406, -104.919831),
+            (39.656707, -104.919964),
         ]
         coords = [Coordinate.from_lat_lon(lat, lon) for lat, lon in lat_longs]
         # Match(road, coordinate, distance)
@@ -114,19 +121,22 @@ class TestLCSSAddMatchForStationary(TestCase):
         expected_matches = deepcopy(matches)
         # now, add the expected points
         m = expected_matches[-1]
-        new_m = m.set_coordinate(Coordinate('new', m.coordinate.geom, m.coordinate.crs))
+        new_m = m.set_coordinate(
+            Coordinate("new", m.coordinate.geom, m.coordinate.crs)
+        )
         expected_matches.append(new_m)
 
         # StationaryIndex( i_index: List[int], c_index: List[Any])
-        stationary_index = [StationaryIndex([-1,len(matches)], [None,'new'])]
+        stationary_index = [StationaryIndex([-1, len(matches)], [None, "new"])]
 
-        resulting_matches = add_matches_for_stationary_points(matches, stationary_index)
+        resulting_matches = add_matches_for_stationary_points(
+            matches, stationary_index
+        )
 
         self.assertListEqual(expected_matches, resulting_matches)
 
     def test_add_matches_one_stationary_point_in_middle(self):
-        ''' Test adding a single stationary point in the middle
-        '''
+        """Test adding a single stationary point in the middle"""
         # Road(id: str|int, geom: LineString, metadata: dict)
         roads = [
             Road("first st", LineString(), {}),
@@ -138,12 +148,12 @@ class TestLCSSAddMatchForStationary(TestCase):
         ]
 
         lat_longs = [
-            (39.655193,-104.919294),
-            (39.655494,-104.91943),
-            (39.655801,-104.919567),
-            (39.656103,-104.919698),
-            (39.656406,-104.919831),
-            (39.656707,-104.919964),
+            (39.655193, -104.919294),
+            (39.655494, -104.91943),
+            (39.655801, -104.919567),
+            (39.656103, -104.919698),
+            (39.656406, -104.919831),
+            (39.656707, -104.919964),
         ]
         coords = [Coordinate.from_lat_lon(lat, lon) for lat, lon in lat_longs]
         # Match(road, coordinate, distance)
@@ -153,30 +163,37 @@ class TestLCSSAddMatchForStationary(TestCase):
         expected_matches = deepcopy(matches)
         # now, add the expected points
         m = expected_matches[-1]
-        new_m = m.set_coordinate(Coordinate('new', m.coordinate.geom, m.coordinate.crs))
+        new_m = m.set_coordinate(
+            Coordinate("new", m.coordinate.geom, m.coordinate.crs)
+        )
         expected_matches[-1:-1] = [new_m]
 
         # StationaryIndex( i_index: List[int], c_index: List[Any])
-        stationary_index = [StationaryIndex([-1,-1], [None,'new'])]
+        stationary_index = [StationaryIndex([-1, -1], [None, "new"])]
 
-        resulting_matches = add_matches_for_stationary_points(matches, stationary_index)
+        resulting_matches = add_matches_for_stationary_points(
+            matches, stationary_index
+        )
 
         self.assertListEqual(expected_matches, resulting_matches)
 
         expected_matches = deepcopy(matches)
         indx = len(matches) // 2
         m = expected_matches[indx]
-        new_m = m.set_coordinate(Coordinate('new', m.coordinate.geom, m.coordinate.crs))
+        new_m = m.set_coordinate(
+            Coordinate("new", m.coordinate.geom, m.coordinate.crs)
+        )
         expected_matches.insert(indx, new_m)
-        stationary_index = [StationaryIndex([indx, indx], [None,'new'])]
+        stationary_index = [StationaryIndex([indx, indx], [None, "new"])]
 
-        resulting_matches = add_matches_for_stationary_points(matches, stationary_index)
+        resulting_matches = add_matches_for_stationary_points(
+            matches, stationary_index
+        )
 
         self.assertListEqual(expected_matches, resulting_matches)
 
     def test_add_matches_multiple_stationary_points(self):
-        ''' Test adding multiple stationary points
-        '''
+        """Test adding multiple stationary points"""
         # Road(id: str|int, geom: LineString, metadata: dict)
         roads = [
             Road("first st", LineString(), {}),
@@ -188,32 +205,40 @@ class TestLCSSAddMatchForStationary(TestCase):
         ]
 
         lat_longs = [
-            (39.655193,-104.919294),
-            (39.655494,-104.91943),
-            (39.655801,-104.919567),
-            (39.656103,-104.919698),
-            (39.656406,-104.919831),
-            (39.656707,-104.919964),
+            (39.655193, -104.919294),
+            (39.655494, -104.91943),
+            (39.655801, -104.919567),
+            (39.656103, -104.919698),
+            (39.656406, -104.919831),
+            (39.656707, -104.919964),
         ]
         coords = [Coordinate.from_lat_lon(lat, lon) for lat, lon in lat_longs]
         # Match(road, coordinate, distance)
-        matches: list[Match] = [Match(r, c, 0.1) for r, c in zip(roads, coords)]
+        matches: list[Match] = [
+            Match(r, c, 0.1) for r, c in zip(roads, coords)
+        ]
 
         # ensure that the expected matches are different from the matches that will be passed in
         expected_matches = deepcopy(matches)
         # now, add the expected points
         indx = len(matches) // 2
         m = expected_matches[indx]
-        coord_ids = ['alpha', 'beta', 'gamma']
+        coord_ids = ["alpha", "beta", "gamma"]
         new_matches = [
-            m.set_coordinate(Coordinate(id, m.coordinate.geom, m.coordinate.crs))
+            m.set_coordinate(
+                Coordinate(id, m.coordinate.geom, m.coordinate.crs)
+            )
             for id in coord_ids
         ]
-        expected_matches[indx+1:indx+1] = new_matches
+        expected_matches[indx + 1 : indx + 1] = new_matches
 
         # StationaryIndex( i_index: List[int], c_index: List[Any])
-        stationary_index = [StationaryIndex([indx, indx+1], [None] + coord_ids)]
+        stationary_index = [
+            StationaryIndex([indx, indx + 1], [None] + coord_ids)
+        ]
 
-        resulting_matches = add_matches_for_stationary_points(matches, stationary_index)
+        resulting_matches = add_matches_for_stationary_points(
+            matches, stationary_index
+        )
 
         self.assertListEqual(expected_matches, resulting_matches)
