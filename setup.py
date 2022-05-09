488254e9f8275f4733af2c22d9d0555b617a3303
jhoshiko <jhoshiko@nrel.gov>
2022-05-09 12:26:43 -0600

    bump version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="mappymatch",
-    version="0.2.0",
+    version="0.2.1",
     description="mappymatch is a package for map matching",
     long_description=long_description,
     long_description_content_type="text/markdown",
