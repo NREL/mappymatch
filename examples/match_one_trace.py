aa071f6a4f5d8a75a91d999d0df2a8ea7a6edda2
Nicholas Reinicke <Nicholas.Reinicke@nrel.gov>
2022-10-06 16:32:28 -0600

    add print statements to example

--- examples/match_one_trace.py
+++ examples/match_one_trace.py
@@ -13,15 +13,19 @@ if PLOT:
 
     from mappymatch.utils.plot import plot_geofence, plot_matches, plot_trace
 
+print("loading trace.")
 trace = Trace.from_csv(package_root() / "resources/traces/sample_trace_1.csv")
 
 # generate a geofence polygon that surrounds the trace; units are in meters;
 # this is used to query OSM for a small map that we can match to
+print("building geofence.")
 geofence = geofence_from_trace(trace, padding=1e3)
 
 # uses osmnx to pull a networkx map from the OSM database
+print("pull osm map.")
 nx_map = read_osm_nxmap(geofence)
 
+print("matching .")
 matcher = LCSSMatcher(nx_map)
 
 matches = matcher.match_trace(trace)
